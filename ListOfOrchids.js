06c2d90bbe9720f6e62f77c7a20edb835dcdf47f
PanpaKawaii <dngngchitriu2004@gmail.com>
2024-09-17 16:15:06 +0700

    Add files via upload

---
+++ ListOfOrchids.js
@@ -0,0 +1,179 @@
+import id1 from '../../assets/OrchidImages/1.jpg';
+import id2 from '../../assets/OrchidImages/2.jpg';
+import id3 from '../../assets/OrchidImages/3.jpg';
+import id4 from '../../assets/OrchidImages/4.jpg';
+import id5 from '../../assets/OrchidImages/5.jpg';
+import id6 from '../../assets/OrchidImages/6.jpg';
+import id7 from '../../assets/OrchidImages/7.jpg';
+import id8 from '../../assets/OrchidImages/8.jpg';
+import id9 from '../../assets/OrchidImages/9.jpg';
+import id10 from '../../assets/OrchidImages/10.jpg';
+import id11 from '../../assets/OrchidImages/11.jpg';
+import id12 from '../../assets/OrchidImages/12.jpg';
+import id13 from '../../assets/OrchidImages/13.jpg';
+import id14 from '../../assets/OrchidImages/14.jpg';
+import id15 from '../../assets/OrchidImages/15.jpg';
+import id16 from '../../assets/OrchidImages/16.jpg';
+
+export const Orchids = [
+    {
+        Id: '1',
+        name: 'Taichung Beauty',
+        rating: 5,
+        isSpecial: true,
+        img: id1,
+        color: 'pink',
+        origin: 'Taiwan',
+        category: 'Cattleya'
+    },
+    {
+        Id: '2',
+        name: 'Golden Emperor',
+        rating: 4,
+        isSpecial: false,
+        img: id2,
+        color: 'yellow',
+        origin: 'Thailand',
+        category: 'Dendrobium'
+    },
+    {
+        Id: '3',
+        name: 'Lady of the Night',
+        rating: 5,
+        isSpecial: true,
+        img: id3,
+        color: 'white',
+        origin: 'Brazil',
+        category: 'Brassavola'
+    },
+    {
+        Id: '4',
+        name: 'Phalaenopsis Orchid',
+        rating: 4,
+        isSpecial: false,
+        img: id4,
+        color: 'purple',
+        origin: 'Indonesia',
+        category: 'Phalaenopsis'
+    },
+    {
+        Id: '5',
+        name: 'Sharry Baby',
+        rating: 5,
+        isSpecial: true,
+        img: id5,
+        color: 'red',
+        origin: 'Mexico',
+        category: 'Oncidium'
+    },
+    {
+        Id: '6',
+        name: 'Vanda Blue Magic',
+        rating: 4,
+        isSpecial: false,
+        img: id6,
+        color: 'blue',
+        origin: 'India',
+        category: 'Vanda'
+    },
+    {
+        Id: '7',
+        name: 'Miltassia Shelob',
+        rating: 3,
+        isSpecial: false,
+        img: id7,
+        color: 'brown and yellow',
+        origin: 'Peru',
+        category: 'Miltassia'
+    },
+    {
+        Id: '8',
+        name: 'Zygopetalum',
+        rating: 4,
+        isSpecial: false,
+        img: id8,
+        color: 'green with purple spots',
+        origin: 'South America',
+        category: 'Zygopetalum'
+    },
+    {
+        Id: '9',
+        name: 'Paphiopedilum Maudiae',
+        rating: 5,
+        isSpecial: true,
+        img: id9,
+        color: 'green and white',
+        origin: 'China',
+        category: 'Paphiopedilum'
+    },
+    {
+        Id: '10',
+        name: 'Miltonia Sunset',
+        rating: 4,
+        isSpecial: false,
+        img: id10,
+        color: 'yellow and orange',
+        origin: 'Brazil',
+        category: 'Miltonia'
+    },
+    {
+        Id: '11',
+        name: 'Bulbophyllum Daisy Chain',
+        rating: 3,
+        isSpecial: false,
+        img: id11,
+        color: 'white and purple',
+        origin: 'Australia',
+        category: 'Bulbophyllum'
+    },
+    {
+        Id: '12',
+        name: 'Lycaste Skinneri',
+        rating: 5,
+        isSpecial: true,
+        img: id12,
+        color: 'white',
+        origin: 'Guatemala',
+        category: 'Lycaste'
+    },
+    {
+        Id: '13',
+        name: 'Coelogyne Cristata',
+        rating: 4,
+        isSpecial: false,
+        img: id13,
+        color: 'white with yellow throat',
+        origin: 'Nepal',
+        category: 'Coelogyne'
+    },
+    {
+        Id: '14',
+        name: 'Cymbidium Kiwi Midnight',
+        rating: 5,
+        isSpecial: true,
+        img: id14,
+        color: 'black and green',
+        origin: 'New Zealand',
+        category: 'Cymbidium'
+    },
+    {
+        Id: '15',
+        name: 'Dendrobium Nobile',
+        rating: 4,
+        isSpecial: false,
+        img: id15,
+        color: 'pink and white',
+        origin: 'Himalayas',
+        category: 'Dendrobium'
+    },
+    {
+        Id: '16',
+        name: 'Brassia Rex',
+        rating: 5,
+        isSpecial: true,
+        img: id16,
+        color: 'yellow and brown',
+        origin: 'Ecuador',
+        category: 'Brassia'
+    }
+]
